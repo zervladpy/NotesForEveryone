ea3346e25708d8cca08e686aaed05e599632683f
sabela <sabela@a21eql00.sanclemente.local>
2023-11-20 14:32:54 +0100

    cmopose

---
+++ UF1_Proyecto_compose/settings.gradle.kts
@@ -0,0 +1,18 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+rootProject.name = "UF1_Proyecto_compose"
+include(":app")
+ 
