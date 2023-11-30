6d857fd9e40e2a7fd4244b6198d463f3083e09ff
zervladpy <zernovvladislav7@gmail.com>
2023-11-30 08:38:49 +0100

    (update) Started with ust

--- UF1_Proyecto_compose/app/src/main/java/com/example/uf1_proyecto_compose/data/repository/TaskRepositoryImpl.kt
+++ UF1_Proyecto_compose/app/src/main/java/com/example/uf1_proyecto_compose/data/repository/TaskRepositoryImpl.kt
@@ -1,7 +1,6 @@
 package com.example.uf1_proyecto_compose.data.repository
 
-import androidx.compose.runtime.State
-import androidx.compose.runtime.mutableStateOf
+import androidx.lifecycle.MutableLiveData
 import com.example.uf1_proyecto_compose.data.local.dao.AppDao
 import com.example.uf1_proyecto_compose.data.remote.dto.toDomain
 import com.example.uf1_proyecto_compose.data.remote.task.TaskApi
@@ -16,26 +15,66 @@ class TaskRepositoryImpl
     private val dao: AppDao,
 ) : TaskRepository {
 
-    private val _tasks = mutableStateOf(emptyList<Task>())
-    val tasks: State<List<Task>> = _tasks
+    val tasks: MutableLiveData<List<Task>> by lazy {
+        MutableLiveData<List<Task>>()
+    }
 
     override suspend fun apiGetAll(userUid: String): List<Task> {
-        return api.getAllTasks(userUid).map { it.toDomain() }
+        val result = api.getAllTasks(userUid).map { it.toDomain() }
+        // insert to db
+
+        // set live data
+
+        tasks.value = result
+
+        // return from db
+
+
+        return result
     }
 
     override suspend fun apiGetOne(userUid: String, taskUid: String): Task {
-        return api.getOne(userUid, taskUid).toDomain()
+        val result = api.getOne(userUid, taskUid).toDomain()
+
+        val updatedResult = tasks.value?.map {
+            if (it.uid == result.uid) {
+                result
+            } else it
+        }
+
+        tasks.value = updatedResult
+
+        return result
     }
 
     override suspend fun apiInsert(userUid: String, task: Task) {
+
+        val updatedResult = tasks.value?.toMutableList()!!.apply { add(task) }.toList()
+
+        tasks.value = updatedResult
+
         api.insertTask(userUid, task = task.toDto())
     }
 
     override suspend fun apiUpdate(userUid: String, task: Task) {
+
+        val updatedResult = tasks.value?.map {
+            if (it.uid == task.uid) {
+                task
+            } else it
+        }
+
+        tasks.value = updatedResult
+
         api.updateTask(userUid, task = task.toDto())
     }
 
     override suspend fun apiDeleteById(userUid: String, taskUid: String) {
+
+        val updatedResult = tasks.value?.filter { it.uid == taskUid }
+
+        tasks.value = updatedResult
+
         api.deleteTask(userUid, taskUid)
     }
 
