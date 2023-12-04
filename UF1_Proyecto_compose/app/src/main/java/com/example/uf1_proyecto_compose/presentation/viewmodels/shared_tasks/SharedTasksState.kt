3606354159dc8caafaa619f8fc660c43737b427a
zervladpy <zernovvladislav7@gmail.com>
2023-12-04 11:40:42 +0100

    (udpate) readmes

--- UF1_Proyecto_compose/app/src/main/java/com/example/uf1_proyecto_compose/presentation/viewmodels/shared_tasks/SharedTasksState.kt
+++ UF1_Proyecto_compose/app/src/main/java/com/example/uf1_proyecto_compose/presentation/viewmodels/shared_tasks/SharedTasksState.kt
@@ -10,6 +10,4 @@ data class SharedTasksState(
     val isLoading: Boolean = false,
     val user: User = User(),
     val tasks: List<Task> = emptyList(),
-    val searchTerm: String = "",
-    val searchList: List<Task> = emptyList()
 )
